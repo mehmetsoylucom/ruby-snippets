f52e01d3a89da6f1a407ef8169611404aa4f3b7a
Mehmet SOYLU <m.ssoylu@gmail.com>
2016-05-12 23:35:50 +0000

    To do list task status toggle added

--- todo.rb
+++ todo.rb
@@ -9,6 +9,7 @@ module Menu
       4) Delete
       5) Write a file
       6) Read from a file
+      7) Toggle status for a task
       Q) Quit "
   end
   
@@ -49,26 +50,53 @@ class List
     all_tasks[task_number - 1]  = task
   end
   
+  def toggle(task_number)
+    @all_tasks[task_number - 1].toggle_status
+  end
+  
   def write_to_file(filename)
-    IO.write(filename, @all_tasks.map(&to_s).join("\n"))
+    machinified = @all_tasks.map(&:to_machine).join("\n")
+    IO.write(filename, machinified)
   end
   
   def read_from_file(filename)
     IO.readlines(filename).each do |line|
-      add(Task.new(line.chomp))
+      status, *description = line.split(':')
+      status = status.include?('X')
+      add(Task.new(description.join(':').strip , status))
     end
   end
 end
 
 class Task
   attr_reader :description
+  attr_accessor :completed_status
   
-  def initialize(description)
+  def initialize(description, completed_status = false)
     @description = description
+    @completed_status = completed_status
   end
   
   def to_s
     description
+  end
+  
+  def toggle_status
+    @completed_status = !completed?
+  end
+  
+  def completed?    
+    completed_status
+  end
+  
+  def to_machine
+    "#{represent_status} : #{description}"
+  end
+  
+  private
+  
+  def represent_status
+    "#{completed? ? '[X]' : '[ ]'}"
   end
 end
 
@@ -92,7 +120,7 @@ if __FILE__ == $PROGRAM_NAME
       
     when '3'
       puts my_list.show 
-      my_list.update(prompt('Task number for update').to_i, prompt('New content for task'))
+      my_list.update(prompt('Task number for update').to_i, Task.new(prompt('New content for task')))
     when '4'
       puts my_list.show
       my_list.delete(prompt('Task number for delete').to_i)
@@ -106,6 +134,9 @@ if __FILE__ == $PROGRAM_NAME
       rescue ErrNo::ENOENT
         puts 'File name not found, please verify your file name and path.'
       end
+    when '7'
+      puts my_list.show
+      my_list.toggle(prompt('What is the new status').to_i)
     else
       puts 'Sorry, I did not understand'       
     end 
