81408eb439904518d4908722cd461220032c773c
Mehmet SOYLU <m.ssoylu@gmail.com>
2016-05-19 10:17:29 +0000

    another ternary added

--- 6 - ternary-unary.rb
+++ 6 - ternary-unary.rb
@@ -4,6 +4,8 @@ p 4 < 3 ? '4 smaller 3' : '4 greater 3'
 
 p "answered to the to_s" if 4.respond_to?('to_s') # without else block
 
+p  4.respond_to?('to_s') && "answered to the to_s" # without else block
+
 # Unary
 
 a = 10
