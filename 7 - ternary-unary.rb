bead8134a76ac13913b584ba19cc80e4c4ce5892
Mehmet SOYLU <m.ssoylu@gmail.com>
2016-05-19 09:18:03 +0000

    ternary and unary

---
+++ 7 - ternary-unary.rb
@@ -0,0 +1,20 @@
+# Ternary
+
+p 4 < 3 ? '4 smaller 3' : '4 greater 3'
+
+# Unary
+
+a = 10
+
+# Each row change the a value
+
+p 'a +=2 ' , a +=2
+
+p 'a -=2 ' , a -=2
+
+p 'a *=2' , a *=2
+
+p 'a **=2' , a **=2
+
+p 'a /=2' , a /=2
+ 
