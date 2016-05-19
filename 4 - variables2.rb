0f75647d14ab0fa014501c59f541434052980e0b
Mehmet SOYLU <m.ssoylu@gmail.com>
2016-05-19 08:32:25 +0000

    global variables

--- 4 - variables2.rb
+++ 4 - variables2.rb
@@ -1,9 +1,19 @@
 # Variables 2
 
+# multiple assignment
+
 x, y, z = 5, 11, 88
 
 puts x # 5
 puts y # 11
 puts z # 88
- 
 
+# Ruby predefined global variables
+
+puts global_variables
+
+class Theclass
+    def index
+        self.index
+    end
+end
