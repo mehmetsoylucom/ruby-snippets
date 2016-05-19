8408464d925b30a687e7713fb4d7834c62d04cc3
Mehmet SOYLU <m.ssoylu@gmail.com>
2016-05-19 10:12:50 +0000

    blocks added

--- 9 - block.rb
+++ 9 - block.rb
@@ -1,1 +1,13 @@
 # Block
+
+family_members = ["Yeşim", "Ezel", "Uğur", "Ömer"]
+
+# multirow block
+
+family_members.each do |member_name|
+  puts member_name
+end
+
+# short block
+
+family_members.each { |member_name| puts member_name }
