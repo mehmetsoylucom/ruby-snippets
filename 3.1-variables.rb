ab7718c79c369ce6de260b40af04bb00e82c2de1
mehmetsoylucom <mssoylu@gmail.com>
2016-06-13 16:05:28 +0300

    Line numbers added

--- 3.1-variables.rb
+++ 3.1-variables.rb
@@ -1,41 +1,46 @@
 # Numeric variable
 
 variableNumericName = 5
-puts "numeric variable : " ,  variableNumericName
+puts "Numeric variable (#{__LINE__}): " ,  variableNumericName
 
 input = gets.chomp
 
 # String variable
 
 variableStringName = "Adet"
-puts "string variable : " , variableStringName
+puts "string variable (#{__LINE__}): " , variableStringName
 
 input = gets.chomp
 
 # Concat variables
 
 variableInVariable = "#{variableNumericName} #{variableStringName} "
-puts "Concat variables : ",  variableInVariable
+puts "Concat variables (#{__LINE__}):\n",  variableInVariable
 
 input = gets.chomp
 
 # Manipulate vars in quote 
 
-variableInVariable = "#{variableNumericName*2} #{variableStringName} "
+variableInVariable = "Manupulated variable in quotes (#{__LINE__}) :\n#{variableNumericName*2} #{variableStringName} "
 puts variableInVariable
 
 input = gets.chomp
 
 # Global & local variable
 
+puts "Global variable value (#{__LINE__}):"
 $globalVariable = "Mehmet"
-puts "Merhaba #{$globalVariable}"
+puts "#{$globalVariable}"
+
+input = gets.chomp
 
 def say_hi
-    localVariable = 'I am the localVariable of say_hi'
-    puts "Merhaba #{$globalVariable} ( say_hi output )"
-    puts "#{localVariable}"
+    localVariable = "I am the localVariable of say_hi"
+    puts "Global variable concated in def :\nMerhaba #{$globalVariable} ( from say_hi output )\n\n"
+    puts "Local variable value (#{__LINE__}):\n#{localVariable}"
 end
+
+input.chomp
 
 say_hi
 
@@ -45,8 +50,7 @@ input = gets.chomp
 
 MyConstant = 10
 
-puts "MyConstant value is : #{MyConstant}" 
-
+puts "MyConstant value is :\n#{MyConstant}" 
 
 input = gets.chomp
 
@@ -54,6 +58,10 @@ input = gets.chomp
 
 puts "Variable type for constant : ", defined?(MyConstant) # contant
 
+input = gets.chomp
+
 puts "Variable type for local variable : ", defined?(variableStringName) # local-variable
+
+input = gets.chomp
 
 puts "Variable type for method : ", defined?(say_hi) # method
