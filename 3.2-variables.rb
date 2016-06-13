9e67843dd8030c0205a12b0183a71422a6d241ce
mehmetsoylucom <mssoylu@gmail.com>
2016-06-13 16:03:49 +0300

    Multiple assignment

--- 3.2-variables.rb
+++ 3.2-variables.rb
@@ -1,11 +1,22 @@
 # Variables 2
 
 # multiple assignment
-
+puts "Multiple variable assignment :\nx, y, z = 5, 11, 88"
 x, y, z = 5, 11, 88
 
+input = gets.chomp
+
+puts "Value of X :"
 puts x # 5
+
+input = gets.chomp
+
+puts "Value of Y :"
 puts y # 11
+
+input = gets.chomp
+
+puts "Value of Z :"
 puts z # 88
 
 # Ruby predefined global variables
